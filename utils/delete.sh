d4092daa746c2ae507d8fc19da51a5cf645235e0
Allen Yoo <ayoo123@umd.edu>
2024-10-23 06:31:35 -0400

    Change in Delete.sh

--- utils/delete.sh
+++ utils/delete.sh
@@ -1,19 +1,13 @@
-# kill the tracker.sh and inotifywait processes
-for i in $(sudo ps -e | grep tracker.sh | awk '{print $1}'); do sudo kill $i; done
-for i in $(sudo ps -e | grep inotifywait | awk '{print $1}'); do sudo kill $i; done
+#!/bin/bash
 
-# kill the timer.sh process
+# kill processes
+for i in $(sudo ps -e | grep attacker_status.sh | awk '{print $1}'); do sudo kill $i; done
 for i in $(sudo ps -e | grep timer.sh | awk '{print $1}'); do sudo kill $i; done
 
-# kill the attacker_status.sh process
-for i in $(sudo ps -e | grep attacker_status.sh | awk '{print $1}'); do sudo kill $i; done
-
-# stop all pm2 processes
+#sudo forever stopall
 sudo pm2 stop all
-# remove all pm2 processes
 sudo pm2 delete all
 
 # kill containers
-for i in $(sudo lxc-ls); do sudo lxc-stop -n $i; done
-for i in $(sudo lxc-ls); do sudo lxc-destroy -n $i -f; done
-
+for i in $(sudo lxc-ls); do sudo lxc-stop $i; done
+for i in $(sudo lxc-ls); do sudo lxc-destroy $i -f; done
