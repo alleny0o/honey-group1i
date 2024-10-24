44dc35c5e8b947fe469aa21b1e39a8a34be12cf5
Allen Yoo <ayoo123@umd.edu>
2024-10-24 00:07:33 -0400

    reset.sh update

--- reset.sh
+++ reset.sh
@@ -5,3 +5,5 @@ sudo iptables-restore /home/student/utils/iptables_reset.txt
 
 # stop PM2 process and stop/destroy all containers (and other stuff if needed)
 sudo /home/student/utils/delete.sh
+
+exit 0
